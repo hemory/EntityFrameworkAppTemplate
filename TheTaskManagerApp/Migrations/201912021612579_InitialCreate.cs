9d53604a953dd429bb848ad2d689a89e8c90c2cc
Hemory Phifer <hphifer@uwm.com>
2019-12-02 11:33:18 -0500

    Intitial Commit

---
+++ TheTaskManagerApp/Migrations/201912021612579_InitialCreate.cs
@@ -0,0 +1,27 @@
+namespace TheTaskManagerApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ToDoItems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ToDoItems");
+        }
+    }
+}
---
+++ TheTaskManagerApp/Models/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace TheTaskManagerApp.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ToDoDbContext : DbContext
+    {
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
